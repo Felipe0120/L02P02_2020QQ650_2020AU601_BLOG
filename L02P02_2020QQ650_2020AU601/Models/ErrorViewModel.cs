7557b8c0d19bca8356f05f3afd19e109981f4057
Luis Felipe Quezada Quezada <luisfeli12.lfq@gmail.com>
2023-04-22 08:39:09 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2020QQ650_2020AU601/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2020QQ650_2020AU601.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
