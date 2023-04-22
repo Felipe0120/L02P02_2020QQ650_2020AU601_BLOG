c752f4305575c8e4d20ec355f4ec79bec08701fb
Rodrigo Aguilar <rodrigo.aguilar@catolica.edu.sv>
2023-04-22 09:09:01 -0600

    calificaciones

---
+++ L02P02_2020QQ650_2020AU601/Models/calificaciones.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace L02P02_2020QQ650_2020AU601.Models
+{
+    public class calificaciones
+    {
+        [Key]
+
+        public int calificacionId { get; set; }
+        public int publicacionId { get; set; }
+        public int usuarioId { get; set; }
+        public int calificacion { get; set; }
+    }
+}
---
+++ L02P02_2020QQ650_2020AU601/Models/comentarios.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace L02P02_2020QQ650_2020AU601.Models
+{
+    public class comentarios
+    {
+        [Key]
+
+        public int cometarioId { get; set; }
+        public int publicacionId { get; set; }
+        public string comentario { get; set; }
+        public int usuarioId { get; set; }
+    }
+}
