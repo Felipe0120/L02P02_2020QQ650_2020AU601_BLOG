2caba9af812de18cdded123e919622cbc9f2079f
Luis Felipe Quezada Quezada <luisfeli12.lfq@gmail.com>
2023-04-22 08:55:58 -0600

    blog

---
+++ L02P02_2020QQ650_2020AU601/Models/blogDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+using L02P02_2020QQ650_2020AU601.Models;
+namespace L02P02_2020QQ650_2020AU601.Models
+{
+    public class blogDbContext : DbContext
+    {
+    }
+}
--- L02P02_2020QQ650_2020AU601/Program.cs
+++ L02P02_2020QQ650_2020AU601/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using L02P02_2020QQ650_2020AU601.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<blogDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("equiposDbConnection")));
 
 var app = builder.Build();
 
